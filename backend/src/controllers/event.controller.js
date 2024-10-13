0accf36baf8a373319488478f3bebc21d5dc0bca
Juman Shrabanya <jumanshrabanya.it@gmail.com>
2024-10-13 16:37:58 +0530

    search query implementation

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -9,9 +9,9 @@ const showEvents = asyncHandler(async (req, res) => {
   const events = await Event.find({});
 
   if (!events || events.length === 0) {
-    throw new ApiError(401, "No events found");
+    throw new ApiError(404, "No events found");
   }
-  res.status(200).json(new ApiResponse("Available events: ", events));
+  res.status(200).json(new ApiResponse(201, events));
 });
 
 // to handle the creation of a event
@@ -56,4 +56,27 @@ const createEvent = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse("Event created Successfully", newEvent));
 });
 
-export { showEvents, createEvent };
+// to handle event by search query
+const searchEvent = asyncHandler(async (req, res) => {
+  // get the query params
+  // check if matching event available or not
+  // if available, then showcase it
+  // if not then show nothing
+
+  const { search } = req.query;
+  if (!search) {
+    throw new ApiError(404, "Search query is required");
+  }
+
+  const matchingEvents = await Event.find({
+    title: { $regex: search, $options: "i" },
+  });
+
+  if (matchingEvents.length === 0) {
+    throw new ApiError(404, "No events found");
+  }
+
+  res.status(200).json(new ApiResponse("Found events: ", matchingEvents));
+});
+
+export { showEvents, createEvent, searchEvent };
--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { showEvents, createEvent } from "../controllers/event.controller.js";
+import {
+  showEvents,
+  createEvent,
+  searchEvent,
+} from "../controllers/event.controller.js";
 
 const router = Router();
 
-router.route("/events").post(showEvents);
+router.route("/events").get(showEvents);
 router.route("/create-event").post(createEvent);
+router.route("/").get(searchEvent); //http://localhost:8000/api/v1/event?search=coding
 
 export default router;
